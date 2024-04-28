beeb8a3f336dbff03b0a222d866972d09bed416d
cmaceves <christineaceves22@gmail.com>
2024-04-28 12:34:57 -0700

    parse entities such that all ascendants are included in list of acceptable labels

--- predicates/parse_biolink_predicates.py
+++ predicates/parse_biolink_predicates.py
@@ -13,32 +13,33 @@ with open("biolink-model.yaml", "r") as stream:
         print(exc)
 entity_definition_file = "./entities.json"
 
-"""
-useful_definitions = ["anatomical entity", "anatomical entity to anatomical entity association", "biological entity", "biological process", "biological process or activity", "chemical substance", "disease or phenotypic feature", "gene or gene product", "gene product", "genomic entity","molecular entity", "protein"]
-all_definitions = []
 entities = {}
 for key, value in data['classes'].items():
+    if key not in entities:
+        entities[key] = []
     if 'is_a' in value:
-        all_definitions.append(value['is_a'])
-        if value['is_a'] in useful_definitions:
-            if 'description' in value:
-                entities[key] = value['description']
-                print(key, value)
-#print(np.unique(all_definitions))
+        parent = value['is_a']
+        entities[key].append(parent)
+        while parent in data['classes']:
+            tmp = data['classes'][parent]
+            if 'is_a' in tmp:
+                parent = tmp['is_a']
+                entities[key].append(parent)
+            else:
+                parent = ""
 with open(entity_definition_file, 'w') as jfile:
     json.dump(entities, jfile)
-sys.exit(0)
-"""
-predicate_dict = {}
-useful_is_a = ['node property', 'association slot', 'aggregate statistic', 'interbase coordinate']
-for key, value in data['slots'].items():
 
-    if ('range' not in value or 'domain' not in value) and "is_a" not in value:
+predicate_dict = {}
+useful_is_a = []
+for key, value in data['slots'].items():
+    if key == "node property":
         print(key, value)
-        continue
     if 'is_a' in value and value['is_a'] in useful_is_a:
         #print(key, "-", value['is_a'])
         continue
+    if key == "synonym":
+        print(key, value)
     if key not in predicate_dict:
         predicate_dict[key] = {}
     if "description" in value:
