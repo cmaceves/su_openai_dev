e4b9610bd7a21feeea3dcfd7dd73ebfb5b790056
cmaceves <christineaceves22@gmail.com>
2024-07-22 22:11:21 +0000

    eval

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -6,9 +6,52 @@ import sys
 import json
 import random
 import numpy as np
+import prompts
 import wiki_pipeline
 import post_process_paths
+from line_profiler import LineProfiler
 random.seed(42)
+
+def evaluate_possible_links(paths, triples, iteration_number, current_info):
+    statements = ""
+    track_links = []
+    counter = 1
+    statement_list = []
+    for path in paths:
+        subsection = path[iteration_number-1:iteration_number+1]
+        if subsection in track_links:
+            continue
+        for triple in triples:
+            if triple[0] == subsection[0] and triple[-1] == subsection[1]:
+                track_links.append(subsection)
+                statement_list.append(triple)
+                statements += str(counter) + ". " + current_info + " " + " ".join(triple) + "\n"
+                counter += 1
+
+    return(statements, track_links, statement_list)
+    
+
+def iterative_mechanism_build(slimmed_paths, drug, disease, mechanism, triples):
+    iteration_number = 1
+    keep_going = True
+    full_links = [drug]
+    current_info = ""    
+    while keep_going:
+        statements, track_links, statement_list  = evaluate_possible_links(slimmed_paths, triples, iteration_number, current_info)
+        resp, prompt = prompts.test_choose_link(statements, mechanism, drug, disease)
+        index = int(resp) - 1
+        current_info += " ".join(statement_list[index]) + ", "
+        full_links.append(track_links[index][-1])
+        
+        tmp = []
+        for path in slimmed_paths:
+            if path[iteration_number-1:iteration_number+1] == track_links[index]:
+                tmp.append(path)
+        slimmed_paths = tmp
+        iteration_number += 1
+        if full_links[-1] == disease:
+            keep_going = False
+    return(full_links)
 
 def parse_indication_paths(indication_json, n=10):
     """
@@ -60,31 +103,41 @@ def main():
     evaluation_indications = parse_indication_paths(indication_json, n)
     
     #leave open for parallel process
-    for indication in evaluation_indications:
+    for i, indication in enumerate(evaluation_indications):
         node_names = []
         for node in indication['nodes']:
             node_names.append(node['name'].lower())
+
         print(node_names)
-        
         #actually does the work to pull out the graph
+        """
+        lp = LineProfiler()
+        lp_wrapper = lp(wiki_pipeline.main)
+        lp_wrapper(indication, flatten_meta_categories)
+        lp.print_stats()
+        sys.exit(0)
+        """
         #wiki_pipeline.main(indication, flatten_meta_categories) 
         #sys.exit(0)
-
         basename = node_names[0] + "_" + node_names[-1] + ".json"
         basename = basename.replace(" ","_")
         output_filename = os.path.join(literature_dir, basename)
-
+        if not os.path.isfile(output_filename):
+            sys.exit(0)
+            continue
         with open(output_filename, 'r') as ofile:
             data = json.load(ofile)
         triples = data['triplets']
         entities = data['entities']
         grounded_type = data['grounded_type']
+        mechanism = data['mechanism']
+        print("Entities", entities)
+        print("Mechanism", mechanism)
         paths = post_process_paths.create_graph(entities, triples, node_names[0], node_names[-1])
         slimmed_paths = []
         path_lengths = []
 
-        #print(triples)
-        print(len(paths))
+        print("Full Path Lengths ", len(paths))
         for path in paths:
             path_types = []
             for node in path:                
@@ -106,30 +159,12 @@ def main():
                     break
             if keep:
                 slimmed_paths.append(path)
+        full_links = iterative_mechanism_build(slimmed_paths, node_names[0], node_names[-1], mechanism, triples)
+        print(full_links)
+        data['mechanism'] = full_links
+        with open(output_filename, 'w') as ofile:
+            json.dump(data, ofile)
 
-        print(len(slimmed_paths))
-        print(slimmed_paths[:10])
-        print(triples)
-        string_paths = ""
-        for j, path in enumerate(slimmed_paths):
-            construct_string = ""
-            node_pairs = []
-            for k, node in enumerate(path):
-                if k+1 < len(path):
-                    node_pairs.append([node, path[k+1]])
-            
-            for k, node in enumerate(node_pairs):
-                construct_string += node[0]
-                for trip in triples:
-                    if node[0] == trip[0] and node[-1] == trip[-1]:
-                        construct_string += " " + trip[1] + " " + trip[-1] + ", " 
-                        break
-            #construct_string += path[-1]
-            string_paths += str(j+1) + "." + construct_string + "\n"
-
-        print(string_paths)
-        prompts.choose_path(string_paths, mechanism)
-        sys.exit(0)
         #evaluate based on node groundings
         #evaluate based on predicate similarity
     
